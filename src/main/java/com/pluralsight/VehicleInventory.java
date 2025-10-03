091b815f60237d97c7b082c30e5194168f93b2b3
Ash <mendezashleyy@gmail.com>
2025-10-03 12:19:39 -0400

    just writing stuff down

--- src/main/java/com/pluralsight/VehicleInventory.java
+++ src/main/java/com/pluralsight/VehicleInventory.java
@@ -9,5 +9,37 @@ public class VehicleInventory {
         car[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500.00f);
         car[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500.00f);
         car[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000.00f);
+
+        System.out.println("What do you want to do?");
+        System.out.println(" 1 - List all vehicles");
+        System.out.println(" 2 - Search by make/model");
+        System.out.println(" 3 - Search by price range");
+        System.out.println(" 4 - Search by color");
+        System.out.println(" 5 - Add a vehicle");
+        System.out.println(" 6 - Quit");
+        System.out.println("Enter your command: ");
+        int command = scanner.nextInt();
+        switch(command) {
+            case 1:
+                listAllVehicles();
+                break;
+            case 2:
+                findVehiclesByPrice();
+                break;
+            case 5:
+                addAVehicle();
+                break;
+            case 6:
+                return;
+        }
+
+        public void listAllVehicles(){
+            while(car[i]){
+
+            }
+            System.out.println("");
+        }
+
+
     }
 }
