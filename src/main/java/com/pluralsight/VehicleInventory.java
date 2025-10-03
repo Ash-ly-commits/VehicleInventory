41affe2fb86c466ad0d2d45477425bbf4a31326a
Ash <mendezashleyy@gmail.com>
2025-10-03 15:29:36 -0400

    example 4

--- src/main/java/com/pluralsight/VehicleInventory.java
+++ src/main/java/com/pluralsight/VehicleInventory.java
@@ -1,8 +1,12 @@
 package com.pluralsight;
+import java.util.*;
 
 public class VehicleInventory {
+    private static Scanner scanner = new Scanner(System.in);
+    private static int carCount = 6;
+    private static Vehicle[] car = new Vehicle[20];
+
     public static void main(String[] args) {
-        Vehicle[] car = new Vehicle[20];
         car[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500.00f);
         car[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000.00f);
         car[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700.00f);
@@ -11,19 +15,19 @@ public class VehicleInventory {
         car[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000.00f);
 
         System.out.println("What do you want to do?");
-        System.out.println(" 1 - List all vehicles");
-        System.out.println(" 2 - Search by make/model");
-        System.out.println(" 3 - Search by price range");
-        System.out.println(" 4 - Search by color");
-        System.out.println(" 5 - Add a vehicle");
-        System.out.println(" 6 - Quit");
-        System.out.println("Enter your command: ");
+        System.out.println("\t1 - List all vehicles");
+        System.out.println("\t2 - Search by make/model");
+        System.out.println("\t3 - Search by price range");
+        System.out.println("\t4 - Search by color");
+        System.out.println("\t5 - Add a vehicle");
+        System.out.println("\t6 - Quit");
+        System.out.print("Enter your command: ");
         int command = scanner.nextInt();
-        switch(command) {
+        switch (command) {
             case 1:
                 listAllVehicles();
                 break;
-            case 2:
+            case 3:
                 findVehiclesByPrice();
                 break;
             case 5:
@@ -32,14 +36,44 @@ public class VehicleInventory {
             case 6:
                 return;
         }
-
-        public void listAllVehicles(){
-            while(car[i]){
-
-            }
-            System.out.println("");
+    }
+    public static void listAllVehicles () {
+        int counter = 0;
+        System.out.println("All vehicles: ");
+        while (counter < carCount) {
+            System.out.printf("\tVehicle %d: ID %d, %s, %s, Odometer: %d, Price: %.2f\n",
+                    counter+1,car[counter].getVehicleId(),car[counter].getMakeModel(), car[counter].getColor(),
+                    car[counter].getOdometerReading(), car[counter].getPrice());
+            counter++;
         }
-
-
+    }
+    public static void findVehiclesByPrice () {
+        System.out.print("Enter minimum price for vehicle: ");
+        float min = scanner.nextFloat();
+        System.out.print("Enter maximum price for vehicle: ");
+        float max = scanner.nextFloat();
+        System.out.println("Vehicles in your price range: ");
+        for (int i=0;i<carCount;i++){
+            if ((car[i].getPrice() > min) && (car[i].getPrice() < max)){
+                System.out.printf("Vehicle ID %d, %s, %s, Odometer: %d, Price: %.2f\n",
+                        car[i].getVehicleId(), car[i].getMakeModel(), car[i].getColor(),
+                        car[i].getOdometerReading(), car[i].getPrice());
+            }
+        }
+    }
+    public static void addAVehicle(){
+        car[carCount] = new Vehicle();
+        System.out.print("\nEnter vehicle ID: ");
+        car[carCount].setVehicleId(scanner.nextLong());
+        scanner.nextLine();
+        System.out.print("Enter vehicle make/model: ");
+        car[carCount].setMakeModel(scanner.nextLine());
+        System.out.print("Enter vehicle color: ");
+        car[carCount].setColor(scanner.nextLine());
+        System.out.print("Enter odometer reading: ");
+        car[carCount].setOdometerReading(scanner.nextInt());
+        System.out.print("Enter price: ");
+        car[carCount].setPrice(scanner.nextFloat());
+        carCount++;
     }
 }
